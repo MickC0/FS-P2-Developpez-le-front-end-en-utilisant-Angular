788164ba91c7af4ee26cb8c8aeddd07854542d27
MickC0 <krysnn@gmail.com>
2025-02-01 03:54:25 +0100

    gestion du type et erreur de olympicservice. ajout configuration chart

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject, throwError} from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import {Olympic} from "../models/Olympic";
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<Olympic[]>([]);
 
   constructor(private http: HttpClient) {}
 
@@ -16,11 +17,9 @@ export class OlympicService {
     return this.http.get<any>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
-        return caught;
+        console.error('Erreur lors de la récupération des données :', error);
+        this.olympics$.next([]);
+        return throwError(() => error);
       })
     );
   }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import {Color, ScaleType} from "@swimlane/ngx-charts";
 
 @Component({
     selector: 'app-home',
@@ -10,6 +11,20 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
+
+  //pie chart
+  public chartData: {name: string, value: number}[] = [];
+  public single: {name: string, value: number}[] = [];
+  public view: [number, number] = [700, 400];
+  public gradient = false;
+  public labels = true;
+
+  public colorScheme: Color = {
+    domain: ['#793D52', '#89A1DA', '#9680A1', '#BEE0F1', '#B8CAE6', '#945F65'],
+    name: 'ordinal',
+    selectable: true,
+    group: ScaleType.Ordinal,
+  };
 
   constructor(private olympicService: OlympicService) {}
 
