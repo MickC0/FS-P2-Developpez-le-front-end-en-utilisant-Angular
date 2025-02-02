25260d5e3f288abb05c11c2425939c18f14e893f
MickC0 <krysnn@gmail.com>
2025-02-02 17:30:15 +0100

    ajout du css de la page accueil. creation du composant detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import {PieChartModule} from "@swimlane/ngx-charts";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule, PieChartModule, FontAwesomeModule],
-  providers: [],
+  declarations: [AppComponent, NotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, PieChartModule, FontAwesomeModule, HomeComponent],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
-import {Color, ScaleType} from "@swimlane/ngx-charts";
-import {IconDefinition} from "@fortawesome/angular-fontawesome";
+import {Color, PieChartModule, ScaleType} from "@swimlane/ngx-charts";
+import {FaIconComponent, IconDefinition} from "@fortawesome/angular-fontawesome";
 import { faMedal } from '@fortawesome/free-solid-svg-icons';
+import {Olympic} from "../../core/models/Olympic";
+import {Router} from "@angular/router";
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    standalone: false
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  imports: [
+    PieChartModule,
+    FaIconComponent,
+
+
+
+  ]
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<Olympic[]> = of([]);
 
   //pie chart
   public chartData: {name: string, value: number}[] = [];
-  public single: {name: string, value: number}[] = [];
   public view: [number, number] = [700, 400];
   public gradient = false;
   public labels = true;
+
 
   public colorScheme: Color = {
     domain: ['#793D52', '#89A1DA', '#9680A1', '#BEE0F1', '#B8CAE6', '#945F65'],
@@ -31,9 +39,40 @@ export class HomeComponent implements OnInit {
   //icon
   public faMedal: IconDefinition = faMedal;
 
-  constructor(private olympicService: OlympicService) {}
+  // Exemple de stats à afficher dans les "cards"
+  public totalJOs = 0;        // Valeur d'exemple
+  public totalCountries = 0;  // Valeur d'exemple
+
+  constructor(
+    private olympicService: OlympicService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.olympicService.getOlympics().subscribe({
+      next: (olympics: Olympic[]) => {
+        this.totalCountries = olympics.length;
+        const allYears = new Set<number>();
+        olympics.forEach(o => {
+          o.participations.forEach(p => allYears.add(p.year));
+        });
+        this.totalJOs = allYears.size;
+
+        this.chartData = olympics.map(olympic => {
+          const totalMedals = olympic.participations.reduce(
+            (acc, participation) => acc + participation.medalsCount,
+            0
+          );
+          return { name: olympic.country, value: totalMedals };
+        });
+      },
+      error: (error) => console.error('Erreur lors du chargement des données', error)
+    });
   }
+
+  onSelect(selectedItem: { name: string, value: number }): void {
+    // Par exemple, la route attend un paramètre "country"
+    this.router.navigate(['/detail', selectedItem.name]);
+  }
+
 }
